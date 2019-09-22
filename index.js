b9d008a4305d71dc2959f88895216ff40f299840
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-22 01:51:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,5 +34,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten(name) {
-  var newKittens = kittens.slice(0, kittens.length, )
+  var newKittens = kittens.slice(0, kittens.length - 1);
+  
 }
